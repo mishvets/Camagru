c339b2eb3eac4dd7bb0f99e968826a6f4efca726
mishvets <19mch97@gmail.com>
2019-07-12 16:57:21 +0300

    Registration not finish, add base design for registration,Sticky navbar doesn't work...

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -2,24 +2,27 @@
 <html>
 <head>
     <meta charset="UTF-8">
-<!--    <meta name="viewport"-->
-<!--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">-->
-<!--    <meta http-equiv="X-UA-Compatible" content="ie=edge">-->
+<!--    A viewport meta tag should make the web site work on all devices and screen resolutions:-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/css/style.css">
     <title> <?php echo $title ?></title>
 </head>
 <body>
     <header>
-        <nav class="header">
-            <ul>
-                <li><a href="/account/login">LOG IN</a></li>
-                <li><a href="/account/register">SIGN UP</a></li>
-                <li><a href="index.php">HOME</a></li>
-                <li><a href="logout.php">LOGOUT</a></li>
-            </ul>
+        <nav class="navbar">
+            <a href="#" class="active">Home</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#" class="right">Link</a>
         </nav>
+        <div class="navbar1">
+                <a href="/account/login">LOG IN</a>
+                <a href="/account/register">SIGN UP</a>
+                <a href="index.php">HOME</a>
+                <a href="logout.php">LOGOUT</a>
+        </div>
     </header>
     <?php echo $content ?>
-    <div id="footer">mshvets &#169 2019</div>
+  <div id="footer">mshvets &#169 2019</div>
 </body>
 </html>
