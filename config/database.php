75adcb1b92404c303f047144d9ff34e5fcfa0957
mshvets <19mch97@gmail.com>
2019-08-01 13:03:55 +0300

    Change DB settings for home MAMP

--- config/database.php
+++ config/database.php
@@ -1,8 +1,11 @@
 <?php
 //$DB_DSN = 'mysql:host=localhost;dbname=db_camagru';
-$DB_DSN = 'mysql:host=localhost';
+//$DB_DSN = 'mysql:host=localhost';
+//$DB_USER = 'root';
+//$DB_PASSWORD = 'mysql9';
+$DB_DSN = 'mysql:host=localhost;port=3306';
 $DB_USER = 'root';
-$DB_PASSWORD = 'mysql9';
+$DB_PASSWORD = 'root';
 $DB_OPTIONS = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
